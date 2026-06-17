4f0746436c7e69c92f08ce2ffb2cd84257aa6bde
DTrang0801 <thien.nguyen@student.ehb.be>
2026-06-17 12:05:54 +0200

    Add $fillable property to Article model
    Added mass-assignable fields (title, content, image_path, published_at, author_id, category_id) to allow Article::create() to work properly. Without this, mass assignment would fail silently or throw an error.

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -10,8 +10,16 @@ class Article extends Model
     /** @use HasFactory<\Database\Factories\ArticleFactory> */
     use HasFactory;
 
-    
-        function author()
+    protected $fillable = [
+        'title',
+        'content',
+        'image_path',
+        'published_at',
+        'author_id',
+        'category_id',
+    ];
+
+    function author()
     {
         return $this->belongsTo(User::class, 'author_id', 'id');
     }
